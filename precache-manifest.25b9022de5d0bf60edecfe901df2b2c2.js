93a30e950d463ad7a25cc0e3eef00288261fbe0e
Matei Mircea <mrc_matei@yahoo.com>
2020-07-26 23:43:25 +0300

    Updates

---
+++ precache-manifest.25b9022de5d0bf60edecfe901df2b2c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1726deea9637e5bafd57d51dfcda5d16",
+    "url": "/breakingbad/index.html"
+  },
+  {
+    "revision": "e1d021c899aabdd91de5",
+    "url": "/breakingbad/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "07e440605cf63721af3d",
+    "url": "/breakingbad/static/css/main.b9c6e579.chunk.css"
+  },
+  {
+    "revision": "e1d021c899aabdd91de5",
+    "url": "/breakingbad/static/js/2.def6d669.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breakingbad/static/js/2.def6d669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07e440605cf63721af3d",
+    "url": "/breakingbad/static/js/main.cd5b0aab.chunk.js"
+  },
+  {
+    "revision": "ed6879c640456d485a81",
+    "url": "/breakingbad/static/js/runtime-main.fc7df85d.js"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breakingbad/static/media/logo.3795f66d.png"
+  }
+]);
